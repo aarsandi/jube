74e92006b08247bbdf874fbf764607d577c770be
DESKTOP-TJFF029\mocha <mocharizktama@gmail.com>
2022-07-07 21:02:24 +0700

    Click Humberger

--- src/pages/auth/Homepage.js
+++ src/pages/auth/Homepage.js
@@ -48,7 +48,7 @@ export default function Homepage() {
                 <div className="header-content">
                     <nav className="navbar navbar-expand">
                         <div className="collapse navbar-collapse justify-content-between">
-                            <div className="header-left">
+                            {/* <div className="header-left">
                                 <div className="input-group search-area right d-lg-inline-flex d-none">
                                     <input type="text" className="form-control" placeholder="Find something here..." />
                                     <div className="input-group-append">
@@ -59,15 +59,15 @@ export default function Homepage() {
                                         </span>
                                     </div>
                                 </div>
-                            </div>
+                            </div> 
                             <ul className="navbar-nav header-right main-notification">
                                 
-                                {/* <li className="nav-item dropdown notification_dropdown">
+                                 <li className="nav-item dropdown notification_dropdown">
                                     <a className="nav-link bell dz-fullscreen" href="#">
                                         <svg id="icon-full" viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" className="css-i6dzq1"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3" style="stroke-dasharray: 37, 57; stroke-dashoffset: 0;"></path></svg>
                                         <svg id="icon-minimize" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-minimize"><path d="M8 3v3a2 2 0 0 1-2 2H3m18 0h-3a2 2 0 0 1-2-2V3m0 18v-3a2 2 0 0 1 2-2h3M3 16h3a2 2 0 0 1 2 2v3" style="stroke-dasharray: 37, 57; stroke-dashoffset: 0;"></path></svg>
                                     </a>
-                                </li> */}
+                                </li> 
                                 
                                 <li className="nav-item dropdown header-profile">
                                     <a className="nav-link" href="#" role="button" data-toggle="dropdown">
@@ -93,6 +93,7 @@ export default function Homepage() {
                                     </div>
                                 </li> 
                             </ul>
+							*/}
                         </div>
                     </nav>
                     <div className="sub-header">

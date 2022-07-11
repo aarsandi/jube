56f11f947c08aca72baf3e8b0c3713e811f68496
DESKTOP-TJFF029\mocha <mocharizktama@gmail.com>
2022-07-11 21:45:03 +0700

    Update sidebar dikit

--- src/pages/auth/Homepage.js
+++ src/pages/auth/Homepage.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 export default function Homepage() {
   const [nav, setNav] = useState(true);
+  const [nav1, setNav1] = useState(true);
+  const [nav2, setNav2] = useState(true);
 
   return (
     <div id="main-wrapper" className={nav ? "show" : "show menu-toggle"}>
@@ -43,15 +45,6 @@ export default function Homepage() {
               fill="black"
             />
           </svg>
-          {/* {nav ?    
-                    <svg className="brand-title" width="74" height="22" viewBox="0 0 74 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path className="svg-logo-path" d="M0.784 17.556L10.92 5.152H1.176V1.12H16.436V4.564L6.776 16.968H16.548V21H0.784V17.556ZM25.7399 21.28C24.0785 21.28 22.6599 20.9347 21.4839 20.244C20.3079 19.5533 19.4025 18.6387 18.7679 17.5C18.1519 16.3613 17.8439 15.1293 17.8439 13.804C17.8439 12.3853 18.1519 11.088 18.7679 9.912C19.3839 8.736 20.2799 7.79333 21.4559 7.084C22.6319 6.37467 24.0599 6.02 25.7399 6.02C27.4012 6.02 28.8199 6.37467 29.9959 7.084C31.1719 7.79333 32.0585 8.72667 32.6559 9.884C33.2719 11.0413 33.5799 12.2827 33.5799 13.608C33.5799 14.1493 33.5425 14.6253 33.4679 15.036H22.6039C22.6785 16.0253 23.0332 16.7813 23.6679 17.304C24.3212 17.808 25.0585 18.06 25.8799 18.06C26.5332 18.06 27.1585 17.9013 27.7559 17.584C28.3532 17.2667 28.7639 16.8373 28.9879 16.296L32.7959 17.36C32.2172 18.5173 31.3119 19.46 30.0799 20.188C28.8665 20.916 27.4199 21.28 25.7399 21.28ZM22.4919 12.292H28.8759C28.7825 11.3587 28.4372 10.6213 27.8399 10.08C27.2612 9.52 26.5425 9.24 25.6839 9.24C24.8252 9.24 24.0972 9.52 23.4999 10.08C22.9212 10.64 22.5852 11.3773 22.4919 12.292ZM49.7783 21H45.2983V12.74C45.2983 11.7693 45.1116 11.0693 44.7383 10.64C44.3836 10.192 43.9076 9.968 43.3103 9.968C42.6943 9.968 42.069 10.2107 41.4343 10.696C40.7996 11.1813 40.3516 11.8067 40.0903 12.572V21H35.6103V6.3H39.6423V8.764C40.1836 7.90533 40.949 7.23333 41.9383 6.748C42.9276 6.26267 44.0663 6.02 45.3543 6.02C46.3063 6.02 47.0716 6.19733 47.6503 6.552C48.2476 6.888 48.6956 7.336 48.9943 7.896C49.3116 8.43733 49.517 9.03467 49.6103 9.688C49.7223 10.3413 49.7783 10.976 49.7783 11.592V21ZM52.7548 4.62V0.559999H57.2348V4.62H52.7548ZM52.7548 21V6.3H57.2348V21H52.7548ZM63.4657 6.3L66.0697 10.444L66.3497 10.976L66.6297 10.444L69.2337 6.3H73.8537L68.9257 13.608L73.9657 21H69.3457L66.6017 16.884L66.3497 16.352L66.0977 16.884L63.3537 21H58.7337L63.7737 13.692L58.8457 6.3H63.4657Z" fill="black" />
-                    </svg> :
-                    //check all css nya buat kita override
-                    <svg style={{ display:'none' }} className="brand-title" width="74" height="22" viewBox="0 0 74 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path className="svg-logo-path" d="M0.784 17.556L10.92 5.152H1.176V1.12H16.436V4.564L6.776 16.968H16.548V21H0.784V17.556ZM25.7399 21.28C24.0785 21.28 22.6599 20.9347 21.4839 20.244C20.3079 19.5533 19.4025 18.6387 18.7679 17.5C18.1519 16.3613 17.8439 15.1293 17.8439 13.804C17.8439 12.3853 18.1519 11.088 18.7679 9.912C19.3839 8.736 20.2799 7.79333 21.4559 7.084C22.6319 6.37467 24.0599 6.02 25.7399 6.02C27.4012 6.02 28.8199 6.37467 29.9959 7.084C31.1719 7.79333 32.0585 8.72667 32.6559 9.884C33.2719 11.0413 33.5799 12.2827 33.5799 13.608C33.5799 14.1493 33.5425 14.6253 33.4679 15.036H22.6039C22.6785 16.0253 23.0332 16.7813 23.6679 17.304C24.3212 17.808 25.0585 18.06 25.8799 18.06C26.5332 18.06 27.1585 17.9013 27.7559 17.584C28.3532 17.2667 28.7639 16.8373 28.9879 16.296L32.7959 17.36C32.2172 18.5173 31.3119 19.46 30.0799 20.188C28.8665 20.916 27.4199 21.28 25.7399 21.28ZM22.4919 12.292H28.8759C28.7825 11.3587 28.4372 10.6213 27.8399 10.08C27.2612 9.52 26.5425 9.24 25.6839 9.24C24.8252 9.24 24.0972 9.52 23.4999 10.08C22.9212 10.64 22.5852 11.3773 22.4919 12.292ZM49.7783 21H45.2983V12.74C45.2983 11.7693 45.1116 11.0693 44.7383 10.64C44.3836 10.192 43.9076 9.968 43.3103 9.968C42.6943 9.968 42.069 10.2107 41.4343 10.696C40.7996 11.1813 40.3516 11.8067 40.0903 12.572V21H35.6103V6.3H39.6423V8.764C40.1836 7.90533 40.949 7.23333 41.9383 6.748C42.9276 6.26267 44.0663 6.02 45.3543 6.02C46.3063 6.02 47.0716 6.19733 47.6503 6.552C48.2476 6.888 48.6956 7.336 48.9943 7.896C49.3116 8.43733 49.517 9.03467 49.6103 9.688C49.7223 10.3413 49.7783 10.976 49.7783 11.592V21ZM52.7548 4.62V0.559999H57.2348V4.62H52.7548ZM52.7548 21V6.3H57.2348V21H52.7548ZM63.4657 6.3L66.0697 10.444L66.3497 10.976L66.6297 10.444L69.2337 6.3H73.8537L68.9257 13.608L73.9657 21H69.3457L66.6017 16.884L66.3497 16.352L66.0977 16.884L63.3537 21H58.7337L63.7737 13.692L58.8457 6.3H63.4657Z" fill="black" />
-                    </svg>
-                        }    */}
         </a>
 
         <div className="nav-control">
@@ -68,60 +61,10 @@ export default function Homepage() {
         </div>
       </div>
 
-      {/* {
-                nav ? <h1>sadwad</h1> : <h1>dadada</h1>
-            } */}
-
       <div className="header">
         <div className="header-content">
           <nav className="navbar navbar-expand">
             <div className="collapse navbar-collapse justify-content-between">
-              {/* <div className="header-left">
-                                <div className="input-group search-area right d-lg-inline-flex d-none">
-                                    <input type="text" className="form-control" placeholder="Find something here..." />
-                                    <div className="input-group-append">
-                                        <span className="input-group-text">
-                                            <a href="javascript:void(0)">
-                                                <i className="flaticon-381-search-2"></i>
-                                            </a>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div> 
-                            <ul className="navbar-nav header-right main-notification">
-                                
-                                 <li className="nav-item dropdown notification_dropdown">
-                                    <a className="nav-link bell dz-fullscreen" href="#">
-                                        <svg id="icon-full" viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" className="css-i6dzq1"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3" style="stroke-dasharray: 37, 57; stroke-dashoffset: 0;"></path></svg>
-                                        <svg id="icon-minimize" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-minimize"><path d="M8 3v3a2 2 0 0 1-2 2H3m18 0h-3a2 2 0 0 1-2-2V3m0 18v-3a2 2 0 0 1 2-2h3M3 16h3a2 2 0 0 1 2 2v3" style="stroke-dasharray: 37, 57; stroke-dashoffset: 0;"></path></svg>
-                                    </a>
-                                </li> 
-                                
-                                <li className="nav-item dropdown header-profile">
-                                    <a className="nav-link" href="#" role="button" data-toggle="dropdown">
-                                        <img src={`${process.env.PUBLIC_URL}/assets/images/profile/pic1.jpg`} width="20" alt=""/>
-                                        <div className="header-info">
-                                            <span>Johndoe</span>
-                                            <small>Super Admin</small>
-                                        </div>
-                                    </a>
-                                    <div className="dropdown-menu dropdown-menu-right">
-                                        <a href="#" className="dropdown-item ai-icon">
-                                            <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" className="text-primary" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
-                                            <span className="ml-2">Profile </span>
-                                        </a>
-                                        <a href="#" className="dropdown-item ai-icon">
-                                            <svg id="icon-inbox" xmlns="http://www.w3.org/2000/svg" className="text-success" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
-                                            <span className="ml-2">Inbox </span>
-                                        </a>
-                                        <a href="#" className="dropdown-item ai-icon">
-                                            <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" className="text-danger" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
-                                            <span className="ml-2">Logout </span>
-                                        </a>
-                                    </div>
-                                </li> 
-                            </ul>
-							*/}
             </div>
           </nav>
           <div className="sub-header">
@@ -158,6 +101,33 @@ export default function Homepage() {
                 <span className="nav-text">Products</span>
               </Link>
             </li>
+            
+            <li className={nav1 ? "" : "mm-active"} onClick={() => { setNav1(!nav1); }} >
+              <a className="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                <i className="flaticon-077-menu-1"></i> <span className="nav-text">Barang</span>
+              </a>
+            </li>
+            <li>
+              <ul aria-expanded="false"  className={nav1 ? "mm-collapse" : "mm-collapse mm-show"}>
+                  <li><a href="app-profile.html">Profile</a></li>
+                  <li className={nav2 ? "" : "mm-active"} onClick={() => { setNav2(!nav2); }} >
+                      <a className="has-arrow" href="javascript:void(0);" aria-expanded="false">Project<span className="badge badge-xs badge-danger">New</span></a>
+                      <ul aria-expanded="false" className={nav2 ? "left mm-collapse" : "left mm-collapse mm-show"} onClick={() => { setNav2(!nav2); }} >
+                          <li><a href="project-list.html">Project List</a></li>
+                          <li><a href="project-card.html">Project Card</a></li>
+                      </ul>
+                  </li>
+
+                  <li className={nav2 ? "" : "mm-active"} onClick={() => { setNav2(!nav2); }} >
+                      <a className="has-arrow" href="javascript:void(0);" aria-expanded="false">User<span className="badge badge-xs badge-danger">New</span></a>
+                      <ul aria-expanded="false" className={nav2 ? "left mm-collapse" : "left mm-collapse mm-show"} >
+                          <li><a href="user-list-datatable.html">User List</a></li>
+                          <li><a href="user-list-column.html">User Card</a></li>
+                      </ul>
+                  </li>
+              </ul>
+          </li>
+
             <li className="">
               <Link
                 className="ai-icon"
@@ -170,48 +140,6 @@ export default function Homepage() {
               </Link>
             </li>
 
-            {/* <li>
-                            <a href="#" className="ai-icon" aria-expanded="false">
-                                <i className="flaticon-061-puzzle"></i>
-                                <span className="nav-text">Charts</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="ai-icon" aria-expanded="false">
-                                <i className="flaticon-003-diamond"></i>
-                                <span className="nav-text">Bootstrap</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="ai-icon" aria-expanded="false">
-                                <i className="flaticon-053-heart"></i>
-                                <span className="nav-text">Plugins</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="ai-icon" aria-expanded="false">
-                                <i className="flaticon-381-settings-2"></i>
-                                <span className="nav-text">Widget</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="ai-icon" aria-expanded="false">
-                                <i className="flaticon-044-file"></i>
-                                <span className="nav-text">Forms</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="ai-icon" aria-expanded="false">
-                                <i className="flaticon-381-network"></i>
-                                <span className="nav-text">Table</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="ai-icon" aria-expanded="false">
-                                <i className="flaticon-049-copy"></i>
-                                <span className="nav-text">Pages</span>
-                            </a>
-                        </li> */}
           </ul>
           <div className="copyright">
             <p>
